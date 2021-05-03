9d114df2e93c3155f697817c36b6638eb2cb7f5c
Milton C. S. Junior <miltonc.s.junior@outlook.com>
2021-05-03 16:52:58 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "assets/components/DataTable/DataTable";
+import Footer from "assets/components/Footer/Footer";
+import NavBar from "assets/components/NavBar/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
